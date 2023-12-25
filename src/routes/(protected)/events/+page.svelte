62d684e85df8e1cb56de38dbb8175280f2a37e15
leomet07 <leomet07@gmail.com>
2023-12-24 20:08:41 -0500

    Move events calendar page to main page

--- src/routes/(protected)/events/+page.svelte
+++ src/routes/(protected)/events/+page.svelte
@@ -1,23 +1,65 @@
 <script lang="ts">
-	import { superForm } from "sveltekit-superforms/client";
-	import { currentUser } from "$lib/pocketbase";
-
 	import type { LayoutData } from "./$types";
+	import "@event-calendar/core/index.css";
+	import { goto } from "$app/navigation";
+	import type { RecievedEvent } from "$lib/db_types";
+	import { modeCurrent } from "@skeletonlabs/skeleton";
+	// @ts-ignore
+	import Calendar from "@event-calendar/core";
+	// @ts-ignore
+	import DayGrid from "@event-calendar/day-grid";
+	// @ts-ignore
+	import ListView from "@event-calendar/list";
 
 	export let data: LayoutData;
-	// const { form, errors, constraints } = superForm(data.form);
+
+	// Svelte Calendar Component:
+	// https://github.com/vkurko/calendar
+	let plugins = [DayGrid, ListView];
+	let options = {
+		view: "dayGridMonth",
+		headerToolbar: {
+			start: "prev,next",
+			center: "title",
+			end: "dayGridMonth,listMonth"
+		},
+		events: data.events.map(transformEvent),
+		eventClick: (info: { event: CalendarEvent; el: HTMLElement }) => {
+			console.log(info.event.title);
+			goto("/events/view/" + info.event.id);
+		}
+	};
+
+	type CalendarEvent = {
+		id: string;
+		resourceIds?: string[];
+		allDay?: boolean;
+		start: Date;
+		end: Date;
+		title: string;
+		editable?: boolean;
+		startEditable?: boolean;
+		durationEditable?: boolean;
+		display?: "auto" | "background";
+		backgroundColor?: string;
+		textColor?: string;
+		extendedProps?: any;
+	};
+
+	function transformEvent(event: RecievedEvent): CalendarEvent {
+		return {
+			id: event.id,
+			title: event.name,
+			start: event.start_time,
+			end: event.end_time
+		};
+	}
 </script>
 
-<main class="container mx-auto p-8 space-y-8">
-	<h1 class="h1">Events</h1>
-	<h2 class="h2">Upcoming events:</h2>
-	<section class="grid gap-4 xl:grid-cols-3 md:grid-cols-2">
-		{#each data.events as item}
-			<div class="card p-4">
-				<h3 class="h3 anchor"><a class="ancor" href={`/events/view/${item.id}`}>{item.name}</a></h3>
-				<p class="font-bold">{item.start_time.toDateString()}</p>
-				<p>{item.description}</p>
-			</div>
-		{/each}
-	</section>
+<main class="container mx-auto py-8 px-2 space-y-8">
+	<h1 class="h1">Events General Calendar</h1>
+
+	<div class={!$modeCurrent ? "ec-dark" : ""}>
+		<Calendar {plugins} {options} />
+	</div>
 </main>
