89bf6e3b7d1187ed90d0e12b97451f03d28c7cf6
leomet07 <leomet07@gmail.com>
2024-09-10 09:22:34 -0400

    refactor to mergeEmail lib

--- src/routes/(protected)/admin/+layout.server.ts
+++ src/routes/(protected)/admin/+layout.server.ts
@@ -1,6 +1,7 @@
 import { fail } from "@sveltejs/kit";
-import type { RecievedCredit, RecievedEvent, RecievedPublicUserData, RecievedUser } from "$lib/db_types.js";
+import type { RecievedCredit, RecievedUser } from "$lib/db_types.js";
 import type { LayoutServerLoad } from "./$types";
+import mergeUsersWithEmails from "$lib/mergeUsersWithEmails";
 
 // Get the data, for page load
 export const load: LayoutServerLoad = (async ({ params, locals }) => {
@@ -12,15 +13,13 @@ export const load: LayoutServerLoad = (async ({ params, locals }) => {
         users as unknown
     ) as RecievedUser[];
 
+    const users_with_emails = await mergeUsersWithEmails(serialized_users, locals.pb);
 
-    const users_with_private_emails = (await locals.pb.collection("publicUsers").getFullList()) as unknown as RecievedPublicUserData[];
     const credits = structuredClone(await locals.pb.collection("credits").getFullList() as unknown) as RecievedCredit[];
 
-
-    const serialized_users_with_emails = serialized_users.map(useri => {
+    const serialized_users_with_emails = users_with_emails.map(useri => {
         return {
             ...useri,
-            email: users_with_private_emails.find((privateuser => privateuser.id == useri.id))?.email,
             credits: credits.filter(credit => credit.user == useri.id)
         };
     });
