286325a0044d36dc1da2893e4c0fa052b9cc06d3
leomet07 <leomet07@gmail.com>
2024-09-15 16:52:16 -0400

    fix bug with credits for committee members

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -7,7 +7,7 @@ export const load = (async ({ params, locals }) => {
 	if (!locals.user || locals.user.is_tutee) {
 		return;
 	}
-	const credits = structuredClone(await locals.pb.collection("credits").getFullList() as unknown) as RecievedCredit[];
+	const credits = structuredClone(await locals.pb.collection("credits").getFullList({ "filter": `user="${locals.user.id}"` }) as unknown) as RecievedCredit[];
 
 	return { credits };
 
