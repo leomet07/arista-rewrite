f96056683ee28a480791b71a7611b80f984eab1e
leomet07 <leomet07@gmail.com>
2024-09-22 12:13:21 -0400

    code cleanup

--- pb_hooks/creditTutor.pb.js
+++ pb_hooks/creditTutor.pb.js
@@ -1,4 +1,3 @@
-
 // note, this file and folder must be moved next to the pb_data folder of the pocketbase database folder
 onRecordAfterUpdateRequest(async (e) => {
     const old_record = e.record.originalCopy();
@@ -9,32 +8,14 @@ onRecordAfterUpdateRequest(async (e) => {
         const hours_num = e.record.get("durationInHours");
         const tutor_id = e.record.get("tutor");
 
-        console.log("CREATING CREDITnew: ", JSON.stringify({
-            credits: hours_num,
-            user: tutor_id,
-            event: null,
-            session: e.record.get("id")
-        }))
-
         const creditCollection = $app.dao().findCollectionByNameOrId("credits");
         const newCreditRecord = new Record(creditCollection);
 
         newCreditRecord.set("credits", hours_num);
         newCreditRecord.set("user", tutor_id);
         newCreditRecord.set("event", null);
-        newCreditRecord.set("session", e.record.get("id"));
-
+        newCreditRecord.set("session", e.record.get("id")); // tutoring session id
 
         $app.dao().saveRecord(newCreditRecord);
-
-        // const insert_query = $app.dao().db().insert("credits", {
-        //     credits: hours_num,
-        //     user: tutor_id,
-        //     event: null,
-        //     session: e.record.get("id")
-        // }).execute();
-
-        console.log("After save credit record query");
-
     }
 }, "tutoringSessions")
