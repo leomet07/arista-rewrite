4010e592f81b0a451447074d40484a44728d0e75
leomet07 <leomet07@gmail.com>
2025-01-30 18:08:24 -0500

    Prevent admin page from crashing when opening tutee's page

--- src/routes/(protected)/admin/view_user/[id]/+page.svelte
+++ src/routes/(protected)/admin/view_user/[id]/+page.svelte
@@ -21,57 +21,61 @@
 </script>
 
 <main class="container mx-auto p-8 space-y-8">
-	<h2 class="h2">Viewing {full_user.name}</h2>
+	<h2 class="h2">Viewing: {full_user.name}</h2>
 	<h3 class="h3">Email: {full_user.email}</h3>
-	<CreditsDisplay credits={full_user.credits} user={full_user} />
-	<StrikesDisplay strikes={full_user.strikes} />
-	<form
-		class="card p-4 w-full text-token space-y-4"
-		method="POST"
-		action="?/credit_user"
-		use:enhance
-	>
-		<h3 class="h3">Credit {full_user.name} for:</h3>
-		<InputField
-			label="Enter the # of credits: "
-			placeholder="1"
-			field="credits"
-			form={creditFormObj}
-		/>
-		<InputField
-			label="What is this manual crediting for? Please provide an explanation."
-			placeholder="Tutee mistyped credit, last minute addition to event, etc..."
-			field="manualExplanation"
-			form={creditFormObj}
-		/>
-		<div>
-			<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
-				<RadioItem bind:group={$creditFormType} name="type" value="event">event</RadioItem>
-				<RadioItem bind:group={$creditFormType} name="type" value="tutoring">tutoring</RadioItem>
-				<RadioItem bind:group={$creditFormType} name="type" value="other">other</RadioItem>
-			</RadioGroup>
-		</div>
+	<h3 class="h3">{full_user.name} is a {full_user.is_tutee ? "tutee." : "ARISTA member."}</h3>
 
-		<button type="submit" class="btn variant-filled-secondary">Credit User</button>
-	</form>
-	<form
-		class="card p-4 w-full text-token space-y-4"
-		method="POST"
-		action="?/strike_user"
-		use:enhance
-	>
-		<h3 class="h3">Strike {full_user.name} for:</h3>
-		<InputField
-			label="Reason: "
-			placeholder="Skipping an event, not attending a mandatory meeting..."
-			field="reason"
-			form={strikeFormObj}
-		/>
-		<InputField
-			label="Weight: (as a positive, rational number)"
-			field="weight"
-			form={strikeFormObj}
-		/>
-		<button type="submit" class="btn variant-filled-secondary">Strike User</button>
-	</form>
+	{#if !full_user.is_tutee}
+		<CreditsDisplay credits={full_user.credits} user={full_user} />
+		<StrikesDisplay strikes={full_user.strikes} />
+		<form
+			class="card p-4 w-full text-token space-y-4"
+			method="POST"
+			action="?/credit_user"
+			use:enhance
+		>
+			<h3 class="h3">Credit {full_user.name} for:</h3>
+			<InputField
+				label="Enter the # of credits: "
+				placeholder="1"
+				field="credits"
+				form={creditFormObj}
+			/>
+			<InputField
+				label="What is this manual crediting for? Please provide an explanation."
+				placeholder="Tutee mistyped credit, last minute addition to event, etc..."
+				field="manualExplanation"
+				form={creditFormObj}
+			/>
+			<div>
+				<RadioGroup active="variant-filled-primary" hover="hover:variant-soft-primary">
+					<RadioItem bind:group={$creditFormType} name="type" value="event">event</RadioItem>
+					<RadioItem bind:group={$creditFormType} name="type" value="tutoring">tutoring</RadioItem>
+					<RadioItem bind:group={$creditFormType} name="type" value="other">other</RadioItem>
+				</RadioGroup>
+			</div>
+
+			<button type="submit" class="btn variant-filled-secondary">Credit User</button>
+		</form>
+		<form
+			class="card p-4 w-full text-token space-y-4"
+			method="POST"
+			action="?/strike_user"
+			use:enhance
+		>
+			<h3 class="h3">Strike {full_user.name} for:</h3>
+			<InputField
+				label="Reason: "
+				placeholder="Skipping an event, not attending a mandatory meeting..."
+				field="reason"
+				form={strikeFormObj}
+			/>
+			<InputField
+				label="Weight: (as a positive, rational number)"
+				field="weight"
+				form={strikeFormObj}
+			/>
+			<button type="submit" class="btn variant-filled-secondary">Strike User</button>
+		</form>
+	{/if}
 </main>
