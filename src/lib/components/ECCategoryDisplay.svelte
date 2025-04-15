bf20be38b58193558bba556a8e56190dff514fa3
leomet07 <leomet07@gmail.com>
2025-04-15 09:37:55 -0400

    View entered ecs

---
+++ src/lib/components/ECCategoryDisplay.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	import { extracurriculars } from "$lib/components/ECSelectorStore";
+	import type { ExtraCurricular } from "$lib/db_types";
+	export let formattedName: string;
+	export let category: ExtraCurricular["category"];
+</script>
+
+<div class="card p-4">
+	<h3 class="h3">Your {formattedName}</h3>
+	{#if $extracurriculars}
+		<div class="grid gap-4 xl:grid-cols-3 md:grid-cols-2 mt-4">
+			{#each $extracurriculars as ec}
+				{#if ec.category == category}
+					<div class="card p-4">
+						<h4 class="h4">Organization: {ec.organization}</h4>
+						<p>Position: {ec.position}</p>
+						<p>Description: {ec.description}</p>
+						<p>{ec.hoursPerWeek} hours per week; {ec.weeksPerYear} weeks per year.</p>
+						<p>Faculty Advisor: {ec.advisorName}</p>
+						<p>Faculty Advisor Contact: {ec.advisorContact}</p>
+					</div>
+				{/if}
+			{/each}
+		</div>
+	{:else}
+		<p>You have not added any {formattedName.toLowerCase()}.</p>
+	{/if}
+</div>
--- src/lib/components/ECSelector.svelte
+++ src/lib/components/ECSelector.svelte
@@ -6,6 +6,7 @@
 	import { RadioGroup, RadioItem } from "@skeletonlabs/skeleton";
 	import ErrorComponent from "./ErrorComponent.svelte";
 	import { extracurriculars } from "$lib/components/ECSelectorStore";
+	import ECCategoryDisplay from "./ECCategoryDisplay.svelte";
 
 	let organization = "";
 	let position = "";
@@ -56,56 +57,63 @@
 	}
 </script>
 
-<h3 class="h3">EC Selector</h3>
+<div class="space-y-4">
+	<h3 class="h3">EC Selector</h3>
 
-<!-- Superform is too much, just use svelte stores and validate on server here -->
-<form class="card p-4" on:submit|preventDefault={onAddECButtonClick}>
-	<h1>Create a new EC</h1>
-	<label for="">
-		Organization Name
-		<input class="input" type="text" bind:value={organization} required />
-	</label>
-	<label for="">
-		Your Position
-		<input class="input" type="text" bind:value={position} required />
-	</label>
-	<label for="">
-		Description
-		<input class="input" type="text" bind:value={description} required />
-	</label>
-	<label for="">
-		Hours Per Week
-		<input class="input" inputmode="numeric" type="number" bind:value={hoursPerWeek} required />
-	</label>
-	<label for="">
-		Weeks Per Year
-		<input class="input" inputmode="numeric" type="number" bind:value={weeksPerYear} required />
-	</label>
-	<label for="">
-		Advisor Name
-		<input class="input" type="text" bind:value={advisorName} required />
-	</label>
-	<label for="">
-		Advisor Contact
-		<input class="input" type="text" bind:value={advisorContact} required />
-	</label>
-	<div class="mt-2 mb-2">
-		<h4 class="h4">Please select one category that this extracurricular belongs in.</h4>
-		<RadioGroup rounded="rounded-container-token" flexDirection="flex-col">
-			<RadioItem bind:group={category} name="justify" value={"service_in_stuy"}>
-				Service Activities Inside Stuyvesant
-			</RadioItem>
-			<RadioItem bind:group={category} name="justify" value={"service_out_stuy"}>
-				Service Activities Outside of Stuyvesant
-			</RadioItem>
-			<RadioItem bind:group={category} name="justify" value={"ecs_in_stuy"}>
-				Extracurriculars Activities Inside Stuyvesant
-			</RadioItem>
-			<RadioItem bind:group={category} name="justify" value={"new_type"}>
-				Extracurriculars Activities Outside of Stuyvesant
-			</RadioItem>
-		</RadioGroup>
-	</div>
-	<ErrorComponent {errors} />
-	<button type="submit" class="btn variant-filled">Add Extracurricular</button>
-</form>
+	<ECCategoryDisplay formattedName="In School Service Activities" category="service_in_stuy" />
+	<ECCategoryDisplay formattedName="Out of School Service Activities" category="service_out_stuy" />
+	<ECCategoryDisplay formattedName="In School Extracurriculars" category="ecs_in_stuy" />
+	<ECCategoryDisplay formattedName="Out of School Extracurriculars" category="ecs_out_stuy" />
+
+	<!-- Superform is too much, just use svelte stores and validate on server here -->
+	<form class="card p-4" on:submit|preventDefault={onAddECButtonClick}>
+		<h3 class="h3">Record an Extracurricular</h3>
+		<label for="">
+			Organization Name
+			<input class="input" type="text" bind:value={organization} required />
+		</label>
+		<label for="">
+			Your Position
+			<input class="input" type="text" bind:value={position} required />
+		</label>
+		<label for="">
+			Description
+			<input class="input" type="text" bind:value={description} required />
+		</label>
+		<label for="">
+			Hours Per Week
+			<input class="input" inputmode="numeric" type="number" bind:value={hoursPerWeek} required />
+		</label>
+		<label for="">
+			Weeks Per Year
+			<input class="input" inputmode="numeric" type="number" bind:value={weeksPerYear} required />
+		</label>
+		<label for="">
+			Advisor Name
+			<input class="input" type="text" bind:value={advisorName} required />
+		</label>
+		<label for="">
+			Advisor Contact
+			<input class="input" type="text" bind:value={advisorContact} required />
+		</label>
+		<div class="mt-2 mb-2">
+			<h4 class="h4">Please select one category that this extracurricular belongs in.</h4>
+			<RadioGroup rounded="rounded-container-token" flexDirection="flex-col">
+				<RadioItem bind:group={category} name="justify" value={"service_in_stuy"}>
+					Service Activities Inside Stuyvesant
+				</RadioItem>
+				<RadioItem bind:group={category} name="justify" value={"service_out_stuy"}>
+					Service Activities Outside of Stuyvesant
+				</RadioItem>
+				<RadioItem bind:group={category} name="justify" value={"ecs_in_stuy"}>
+					Extracurriculars Activities Inside Stuyvesant
+				</RadioItem>
+				<RadioItem bind:group={category} name="justify" value={"new_type"}>
+					Extracurriculars Activities Outside of Stuyvesant
+				</RadioItem>
+			</RadioGroup>
+		</div>
+		<ErrorComponent {errors} />
+		<button type="submit" class="btn variant-filled">Add Extracurricular</button>
+	</form>
+</div>
