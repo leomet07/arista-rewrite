67a9a140e9da14798764627b8c90673a90f949f2
leomet07 <leomet07@gmail.com>
2025-04-15 09:52:38 -0400

    mute annoying error in error component

--- src/lib/components/ErrorComponent.svelte
+++ src/lib/components/ErrorComponent.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	export let errors: Record<string, string[]> | undefined;
+	import type { ValidationErrors } from "sveltekit-superforms";
+
+	export let errors: Record<string, string[]> | undefined | ValidationErrors<any>;
+
+	let errors_use: Iterable<any> | undefined = errors as any;
 </script>
 
-{#if errors}
-	{#each Object.entries(errors) as error_parent}
+{#if errors_use}
+	{#each Object.entries(errors_use) as error_parent}
 		{#each error_parent[1] as error}
 			<aside class="alert variant-filled-error mb-4">
 				<b>{error_parent[0] != "_errors" ? `${error_parent[0]}:` : ""} {error}</b>
